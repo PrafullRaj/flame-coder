838b7d40a81906234ff40e6a1578858a2612ca15
kavyabellamkonda123 <114046259+kavyabellamkonda123@users.noreply.github.com>
2022-10-21 17:37:47 +0530

    Create CreateanduseArray.cpp

---
+++ C++/CreateanduseArray.cpp
@@ -0,0 +1,46 @@
+/*Write a Program which creates & uses array of object of a class.( for eg. implementing the 
+list of Managers of a Company having details such as Name, Age, etc..).
+*/
+#include <iostream>
+using namespace std;
+
+class employee
+{
+private:
+    char name[30];
+    float age;
+
+public:
+    void getdata(void);
+    void putdata(void);
+};
+void employee ::getdata(void)
+{
+    cout << "Enter name: ";
+    cin >> name;
+    cout << "Enter age: ";
+    cin >> age;
+}
+
+void employee ::putdata(void)
+{
+    cout << "Name: " << name << "\n";
+    cout << "Age: " << age << "\n";
+}
+int main()
+{
+    int size = 3;
+    employee manager[size];
+    for (int i = 0; i < size; i++)
+    {
+        cout << "\nDetails of manager" << i + 1 << endl;
+        manager[i].getdata();
+    }
+    cout << "\n";
+    for (int i = 0; i < size; i++)
+    {
+        cout << "\nmanager" << i + 1 << "\n";
+        manager[i].putdata();
+    }
+    return 0;
+}
