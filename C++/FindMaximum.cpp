ff8a7e0ae1be62c9a084e72f734268a409dfb1c9
kavyabellamkonda123 <114046259+kavyabellamkonda123@users.noreply.github.com>
2022-10-21 17:39:12 +0530

    Create FindMaximum.cpp

---
+++ C++/FindMaximum.cpp
@@ -0,0 +1,38 @@
+/*Write a Program to find Maximum out of Two Numbers using friend function. Note: Here 
+one number is a member of one class and the other number is member of some other class.*/
+#include<iostream>
+using namespace std;
+
+class ABC;
+class XYZ
+{
+	int x;
+	public:
+		void setvalue(int i){x = i;}
+		friend void max(XYZ, ABC);
+};
+class ABC
+{
+	int a;
+	public:
+		void setvalue(int i){a = i;}
+		friend void max(XYZ, ABC);
+};
+void max(XYZ m, ABC n)	
+{
+	if(m.x >= n.a)
+		cout<< m.x<<endl;
+	else
+		cout<< n.a<<endl;
+}
+int main()
+{
+	ABC abc;
+	abc.setvalue(10);
+	
+	XYZ xyz;
+	xyz.setvalue(20);
+	
+	max(xyz, abc);	
+	return 0;
+}
